eb5d517744c5e348c28d908e893349c5c28337c1
HeyHeyHeyHeyHe <167688019+HeyHeyHeyHeyHe@users.noreply.github.com>
2026-05-28 23:10:18 +0700

    feat: ECG+Delay+Filter+FreeRtos+Queue

--- firmware_esp32/sketch_jan13a.ino
+++ firmware_esp32/sketch_jan13a.ino
@@ -1,82 +1,107 @@
-const int ecgPin = 36;      // SVP
+#define ecgPin 36  // SVP (GPIO36)
 const int LOPlus = 16;
 const int LOMinus = 17;
 
-volatile int ecgValue = 0;
-volatile bool newData = false;
+struct Package {
+  int filteredValue;
+  float bpm;
+};
+QueueHandle_t ecgQueue;
 
-float lowPass = 0.0;
-float highPass = 0.0;
-
-unsigned long lastBeatTime = 0;
-unsigned long firstBeatTime = 0;
-int bpm = 0;
-int beatCount = 0;
-const int threshold = 3800;
-
-void IRAM_ATTR onTimer() {
-  // Check Leads-off
-  if ((digitalRead(LOPlus) == 0) && (digitalRead(LOMinus) == 0)) {
-    ecgValue = analogRead(ecgPin);
-    newData = true;
-  }
-}
+void TaskProcessECG(void *pvParameters);
+void TaskSerialPrint(void *pvParameters);
 
 void setup() {
   Serial.begin(115200);
   pinMode(LOPlus, INPUT);
   pinMode(LOMinus, INPUT);
-  
-  // Setup for Timer Interrupt
-  hw_timer_t * timer = timerBegin(0, 80, true);     // Timer 0, prescaler 80
-  timerAttachInterrupt(timer, &onTimer, true);
-  timerAlarmWrite(timer, 2000, true);               // 2000us = 500Hz
-  timerAlarmEnable(timer);
-  
-  Serial.println("=== ECG Week 2 ===");
-  Serial.println("Collecting sample 500 times/second...");
+  pinMode(ecgPin, INPUT);
+
+  ecgQueue = xQueueCreate(50, sizeof(Package));
+
+  if (ecgQueue != NULL) {
+    xTaskCreatePinnedToCore(
+      TaskProcessECG,
+      "ProcessECG",
+      4096,
+      NULL,
+      3,
+      NULL,
+      1);
+
+    xTaskCreatePinnedToCore(
+      TaskSerialPrint,
+      "SerialPrint",
+      2048,
+      NULL,
+      1,
+      NULL,
+      1);
+  } else {
+    Serial.println("Error creating the queue");
+  }
 }
 
 void loop() {
-  if (newData) {
-    int currentECG = ecgValue; 
-    newData = false;
-    // Can add filter here 
-    lowPass = lowPass * 0.92 + currentECG * 0.08;  // lowPass filter: baseline wander due to breathe
-    highPass = currentECG - lowPass;               // highPass: peaks
-    int filteredValue = (int)(highPass * 8 + 2000);
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+}
 
-    // Calculate Bpm 
-    static bool insidePeak = false; 
+//Task1
+void TaskProcessECG(void *pvParameters) {
+  (void)pvParameters;
+  float lowPass = 0.0;
+  float highPass = 0.0;
+  unsigned long lastPeakTime = 0;
+  unsigned long currentTime = 0;
+  int threshold = 3800;  // Threshold for peak detection
+  int lastValue = 0;
+  int filteredValue=0;
+  float bpm=0.0;
 
-    if (filteredValue > threshold) {
-      if (!insidePeak && (millis() - lastBeatTime > 350)) { //detect peaks
-        insidePeak = true; 
-        if (beatCount == 0) {
-          firstBeatTime = millis(); // Record the first beat in real time 
-        }
-        lastBeatTime = millis();
-        beatCount++;
-        if (beatCount >= 5) {  
-          unsigned long totalDuration = lastBeatTime - firstBeatTime;
-          if (totalDuration > 0) {
-            // Algorithm: (number of times between beats * 60000) / total duration 
-            int calculatedBpm = (4 * 60000) / totalDuration;
-            if (calculatedBpm >= 50 && calculatedBpm <= 140) {
-              bpm = calculatedBpm;
-            }
-          }
-          firstBeatTime = lastBeatTime;
-          beatCount = 1;
+  for (;;) {
+    currentTime = millis();
+    if ((digitalRead(LOPlus) == 0) && (digitalRead(LOMinus) == 0)) {
+      int currentECG = analogRead(ecgPin);
+      
+      lowPass = lowPass * 0.92 + currentECG * 0.08;
+      highPass = currentECG - lowPass;
+      filteredValue = (int)(highPass * 8 + 2000);
+
+      if (filteredValue > threshold && filteredValue > lastValue) {
+        unsigned long timeSinceLastPeak = currentTime - lastPeakTime;
+        if (timeSinceLastPeak > 500) {
+          lastPeakTime = currentTime;
+          bpm = 60000.0 / timeSinceLastPeak;  
         }
       }
+      lastValue = filteredValue;
     } else {
-      insidePeak = false; 
+      filteredValue=0;
+      if (currentTime - lastPeakTime > 4000) {
+        bpm = 0.0; 
+    }} 
+    if (currentTime - lastPeakTime > 3000 && bpm > 0.0) {
+      bpm = 0.0;
     }
+    Package dataToSend;
+    dataToSend.filteredValue = filteredValue;
+    dataToSend.bpm = bpm;
 
-    // Results on Serial Monitor: Filtered Ecg value , Bpm value 
-    Serial.print(filteredValue);
-    Serial.print(",");
-    Serial.println(bpm);
+    xQueueSend(ecgQueue, &dataToSend, 0);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+  }
+}
+
+// Task 2
+void TaskSerialPrint(void *pvParameters) {
+  (void)pvParameters;
+  Package receivedData;
+
+  for (;;) {
+    if (xQueueReceive(ecgQueue, &receivedData, portMAX_DELAY)) {
+      Serial.print(receivedData.filteredValue);
+      Serial.print(",");
+      Serial.println(receivedData.bpm);
+    }
   }
 }
