3aa43a971b30b8520090dcce88bce4cd2d27117c
HeyHeyHeyHeyHe <167688019+HeyHeyHeyHeyHe@users.noreply.github.com>
2026-06-04 12:44:06 +0700

    feat: Detect peak using Adaptive threshold

--- firmware_esp32/sketch_jan13a.ino
+++ firmware_esp32/sketch_jan13a.ino
@@ -1,12 +1,12 @@
 #define ecgPin 36  // SVP (GPIO36)
-const int LOPlus = 16;
-const int LOMinus = 17;
+const int LOPlus = 16; // Leads-off detection positive pin   
+const int LOMinus = 17; // Leads-off detection negative pin
 
 enum HeartStatus {
   NORMAL = 0,
-  BRADYCARDIA = 1, // Bradycardia (< 60 BPM)
-  TACHYCARDIA = 2, // Tachycardia (> 100 BPM)
-  LEADS_OFF = 3   
+  BRADYCARDIA = 1,  // Bradycardia (< 60 BPM)
+  TACHYCARDIA = 2,  // Tachycardia (> 100 BPM)
+  LEADS_OFF = 3
 };
 
 struct Package {
@@ -15,7 +15,7 @@ struct Package {
   float bpm;
   HeartStatus status;
 };
-
+// FreeRTOS Queue Handles
 QueueHandle_t rawEcgQueue;
 QueueHandle_t processedQueue;
 
@@ -28,7 +28,7 @@ void setup() {
   pinMode(LOPlus, INPUT);
   pinMode(LOMinus, INPUT);
   pinMode(ecgPin, INPUT);
-
+  // Initialize FreeRTOS Queues (Capacity: 50 elements each)
   rawEcgQueue = xQueueCreate(50, sizeof(int));
   processedQueue = xQueueCreate(50, sizeof(Package));
 
@@ -61,23 +61,30 @@ void loop() {
   vTaskDelay(1000 / portTICK_PERIOD_MS);
 }
 
-//Task 1
+/**
+ * @brief TASK 1: Data acquisition (ADC sampling)
+ * @details Samples the analog ECG pin at a deterministic 100Hz frequency (10ms intervals)
+ * while managing physical leads-off hardware tracking checks.
+ */
 void TaskADC(void *pvParameters) {
   (void)pvParameters;
   for (;;) {
     int currentECG = 0;
-
+    // Check Leads-off before reading analog value 
     if ((digitalRead(LOPlus) == 0) && (digitalRead(LOMinus) == 0)) {
       currentECG = analogRead(ecgPin);
     } else {
-      currentECG = -1;
+      currentECG = -1; // Flag indicating electrode disconnection
     }
-
     xQueueSend(rawEcgQueue, &currentECG, 0);
     vTaskDelay(10 / portTICK_PERIOD_MS);
   }
 }
-//Task 2
+/**
+ * @brief TASK 2: Signal processing 
+ * @details Implements an integrated real-time QRS detection engine
+ * enhanced with an automated Adaptive Threshold. 
+ */
 #define WINDOW_SIZE 6  //Moving average length
 
 void TaskProcessECG(void *pvParameters) {
@@ -86,7 +93,6 @@ void TaskProcessECG(void *pvParameters) {
   float highPass = 0.0;
   unsigned long lastPeakTime = 0;
   unsigned long currentTime = 0;
-  int threshold = 3800;  // Threshold for peak detection
   int lastValue = 0;
   int filteredValue = 0;
   float bpm = 0.0;
@@ -101,11 +107,17 @@ void TaskProcessECG(void *pvParameters) {
   float derivative = 0.0;
   float squared = 0.0;
 
+  // Variables for adaptive threshold
+  float signalLevel = 57000.0; // estimate for true QRS peak power
+  float noiseLevel = 750.0;
+  float threshold = 15000.0; // initial threshold 
+  int localMax = 0;
+  static bool peakStateRecorded = false; // Flag to check whether the peak is recorded 
   for (;;) {
     if (xQueueReceive(rawEcgQueue, &receivedRaw, portMAX_DELAY)) {
       currentTime = millis();
       if (receivedRaw != -1) {
-        //Baseline wander removal (high-pass) 
+        //Baseline wander removal (high-pass)
         lowPass = lowPass * 0.92 + receivedRaw * 0.08;
         highPass = (float)receivedRaw - lowPass;
 
@@ -123,14 +135,34 @@ void TaskProcessECG(void *pvParameters) {
         moving_average_index = (moving_average_index + 1) % WINDOW_SIZE;
         filteredValue = (int)(moving_sum / (float)WINDOW_SIZE);
 
-        if (filteredValue > threshold && filteredValue > lastValue) {
-          unsigned long timeSinceLastPeak = currentTime - lastPeakTime;
-          if (timeSinceLastPeak > 500) {
-            lastPeakTime = currentTime;
-            bpm = 60000.0 / timeSinceLastPeak;
+        // Adaptive threshold + Bpm calculation
+        if (filteredValue > (int)threshold) {
+          // Identify local maximum point 
+          if (filteredValue < lastValue && !peakStateRecorded) {
+            localMax = lastValue; 
+            // Update values for bpm and signalLevel 
+            unsigned long timeSinceLastPeak = currentTime - lastPeakTime;
+            if (timeSinceLastPeak > 500) {
+              lastPeakTime = currentTime;
+              bpm = 60000.0 / timeSinceLastPeak; 
+              signalLevel = 0.125 * (float)localMax + 0.875 * signalLevel;
+              peakStateRecorded = true; 
+            }
+          }
+        } else  { // Post-peak exit phase 
+          if (peakStateRecorded) {
+            // Reset variables 
+            peakStateRecorded = false; 
+            localMax = 0;
+          }
+          if (filteredValue < (int)(threshold*0.5)){
+          noiseLevel = 0.125 * (float)filteredValue + 0.875 * noiseLevel; 
           }
         }
-        if (currentTime - lastPeakTime > 3500) {
+        threshold = noiseLevel + 0.25 * (signalLevel - noiseLevel);
+
+        // Detect anomal signal
+        if (currentTime - lastPeakTime > 3500) { 
           bpm = 0.0;
           currentStatus = BRADYCARDIA;
         } else if (bpm > 0.0 && bpm < 60.0) {
@@ -147,13 +179,19 @@ void TaskProcessECG(void *pvParameters) {
           bpm = 0.0;
         }
         currentStatus = LEADS_OFF;
-        lowPass = 0.0; 
+        // Reset 
+        lowPass = 0.0;
         prev_highPass = 0.0;
         moving_average_index = 0;
         moving_sum = 0.0;
         for (int i = 0; i < WINDOW_SIZE; i++) {
           moving_average_buffer[i] = 0.0;
         }
+        peakStateRecorded = false;
+        localMax = 0;
+        signalLevel = 4500.0;
+        noiseLevel = 500.0;
+        threshold = 1500.0;
       }
       Package dataToSend;
       dataToSend.rawValue = receivedRaw;
@@ -164,8 +202,10 @@ void TaskProcessECG(void *pvParameters) {
     }
   }
 }
-
-// Task 3
+/**
+ * @brief TASK 3: Output 
+ * @details Dequeues and prints processed ECG data package to the Serial Monitor.  
+ */
 void TaskSerialPrint(void *pvParameters) {
   (void)pvParameters;
   Package receivedData;
@@ -178,15 +218,15 @@ void TaskSerialPrint(void *pvParameters) {
       Serial.print(",");
       Serial.print(receivedData.filteredValue);
       Serial.print(",");
-      Serial.print(receivedData.bpm, 1); 
+      Serial.print(receivedData.bpm, 1);
       Serial.print(",");
-      
-      switch(receivedData.status) {
-        case NORMAL:      Serial.println("NORMAL"); break;
+
+      switch (receivedData.status) {
+        case NORMAL: Serial.println("NORMAL"); break;
         case BRADYCARDIA: Serial.println("BRADYCARDIA"); break;
         case TACHYCARDIA: Serial.println("TACHYCARDIA"); break;
-        case LEADS_OFF:   Serial.println("LEADS_OFF"); break;
+        case LEADS_OFF: Serial.println("LEADS_OFF"); break;
+      }
     }
   }
-}
 }
